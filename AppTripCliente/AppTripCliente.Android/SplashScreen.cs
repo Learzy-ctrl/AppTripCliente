830a2712cc44b50d0b0d7d73d555788d73f339d1
Israel <davidcuriel597@gmail.com>
2023-07-26 15:05:53 -0600

    26/07/23

---
+++ AppTripCliente/AppTripCliente.Android/SplashScreen.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppTripCliente.Droid
+{
+    [Activity(Label = "Trip Inteligente Profesional", Icon = "@drawable/ic_launcher", Theme = "@style/NuevoTema",
+        MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize)]
+    public class SplashScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+            // Create your application here
+        }
+    }
+}
